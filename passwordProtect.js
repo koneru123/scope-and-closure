6b31e043816b6d0d2c3389733552cd2aa5d8c4a9
JohnMichelin <john.michelin@galvanize.com>
2020-02-18 08:43:12 -0800

    Initial commit

---
+++ passwordProtect.js
@@ -0,0 +1,21 @@
+/*
+var myFunc = function(a, b) {
+  console.log('my func ran');
+  return a + b;
+};
+
+var myOtherFunc = function(a, b, c, d, e, f, g, h) {
+  console.log('my func ran');
+  return [a, b, c, d, e, f, g, h];
+};
+
+var protectedMyFunc = passwordProtect(myFunc, 'p@ssw0rd');
+
+protectedMyFunc('password', 1, 2); // nothing logged, nothing returned
+protectedMyFunc('p@ssw0rd', 1, 2); // logs 'my func ran', returns 3
+protectedMyFunc('p@ssw0rd', 13, 5); // logs 'my func ran', returns 18
+*/
+
+var passwordProtect = function(func, password) {
+  // YOUR CODE HERE
+};
